67c19caa05da88f38c769f9e0e844558738d44e1
Ludwin Ramos <lujorabri@gmail.com>
2021-10-29 15:35:02 -0600

    Actualiza Json

--- Main Java/src/main/main.java
+++ Main Java/src/main/main.java
@@ -2,6 +2,6 @@ package main;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo"); //print
     }
 }
